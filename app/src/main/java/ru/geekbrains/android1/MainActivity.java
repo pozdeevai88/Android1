71e84c08290a7bc291628282d777f52dce07b61e
Aleksey <alexcorpetf@gmail.com>
2021-02-28 13:13:21 +1000

    Стабильность
    Повышена стабильность вычислений.  Убраны некоторые ошибки

--- app/src/main/java/ru/geekbrains/android1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/MainActivity.java
@@ -1,24 +1,26 @@
 package ru.geekbrains.android1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.LinkedList;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
 
     static final String LOG_TAG = "CALC_LOG:";
-    static AtomicBoolean result = new AtomicBoolean(false);
-    static AtomicBoolean setAction = new AtomicBoolean(false);
+    public static boolean isResultOnScreen = true;
+    public static boolean isActionSet = false;
     public static String currentNumber = "";
     public static LinkedList<String> formula = new LinkedList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,174 +47,198 @@ public class MainActivity extends AppCompatActivity {
         Button buttonResult = findViewById(R.id.buttonResult);
 
         button1.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "1");
             currentNumber += "1";
         });
 
         button2.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "2");
             currentNumber += "2";
         });
 
         button3.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "3");
             currentNumber += "3";
         });
 
         button4.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "4");
             currentNumber += "4";
         });
 
         button5.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "5");
             currentNumber += "5";
         });
 
         button6.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "6");
             currentNumber += "6";
         });
 
         button7.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "7");
             currentNumber += "7";
         });
 
         button8.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "8");
             currentNumber += "8";
         });
 
         button9.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "9");
             currentNumber += "9";
         });
 
         button0.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + "0");
             currentNumber += "0";
         });
 
         buttonAdd.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            if (setAction.get()) {
-                numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1);
+            if (isResultOnScreen) numbersBox.setText("");
+            if (isActionSet) {
+                try {
+                    numbersBox.setText(
+                            numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1));
+                }catch (StringIndexOutOfBoundsException e) {
+                    numbersBox.setText("error");
+                }
                 formula.removeLast();
             }
             numbersBox.setText(numbersBox.getText() + "+");
-            setAction.set(true);
-            formula.add(currentNumber);
+            if (!isActionSet) formula.add(currentNumber);
+            isActionSet = true;
             formula.add("ADD");
-            Log.i(LOG_TAG, formula.toString());
             currentNumber = "";
         });
 
         buttonSub.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            if (setAction.get()) {
-                numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1);
+            if (isResultOnScreen) numbersBox.setText("");
+            if (isActionSet) {
+                try {
+                    numbersBox.setText(
+                            numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1));
+                }catch (StringIndexOutOfBoundsException e) {
+                    numbersBox.setText("error");
+                }
                 formula.removeLast();
             }
             numbersBox.setText(numbersBox.getText() + "-");
-            setAction.set(true);
-            formula.add(currentNumber);
+            if (!isActionSet) formula.add(currentNumber);
+            isActionSet = true;
             formula.add("SUB");
             currentNumber = "";
         });
 
         buttonMul.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            if (setAction.get()) {
-                numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1);
+            if (isResultOnScreen) numbersBox.setText("");
+            if (isActionSet) {
+                try {
+                    numbersBox.setText(
+                            numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1));
+                }catch (StringIndexOutOfBoundsException e) {
+                    numbersBox.setText("error");
+                }
                 formula.removeLast();
             }
             numbersBox.setText(numbersBox.getText() + "*");
-            setAction.set(true);
-            formula.add(currentNumber);
+            if (!isActionSet) formula.add(currentNumber);
+            isActionSet = true;
             formula.add("MUL");
             currentNumber = "";
         });
 
         buttonDiv.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            if (setAction.get()) {
-                numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1);
+            if (isResultOnScreen) numbersBox.setText("");
+            if (isActionSet) {
+                try {
+                    numbersBox.setText(
+                            numbersBox.getText().subSequence(0, numbersBox.getText().length() - 1));
+                }catch (StringIndexOutOfBoundsException e) {
+                    numbersBox.setText("error");
+                }
                 formula.removeLast();
             }
             numbersBox.setText(numbersBox.getText() + "/");
-            setAction.set(true);
-            formula.add(currentNumber);
+            if (!isActionSet) formula.add(currentNumber);
+            isActionSet = true;
             formula.add("DIV");
             currentNumber = "";
         });
 
         buttonDot.setOnClickListener(v -> {
-            if (result.get()) numbersBox.setText("");
-            result.set(false);
-            setAction.set(false);
+            if (isResultOnScreen) numbersBox.setText("");
+            isResultOnScreen = false;
+            isActionSet = false;
             numbersBox.setText(numbersBox.getText() + ",");
-            currentNumber += ",";
+            currentNumber += ".";
         });
 
         buttonResult.setOnClickListener(v -> {
             formula.add(currentNumber);
-            result.set(true);
+            isResultOnScreen = true;
             currentNumber = "";
             Log.i(LOG_TAG, formula.toString());
-
-            for (int i = 1; i <= formula.size() - 1; i += 2) {
-                Log.i(LOG_TAG, formula.toString());
-                Log.i(LOG_TAG, formula.get(i));
-                Log.i(LOG_TAG, "a = " + formula.get(i - 1));
-                Log.i(LOG_TAG, "b = " + formula.get(i + 1));
-                float a = Float.parseFloat(formula.get(i - 1));
-                float b = Float.parseFloat(formula.get(i + 1));
-                if (formula.get(i).equals("ADD")) {
-                    formula.set(i + 1, String.valueOf(a + b));
-                } else if (formula.get(i).equals("SUB")) {
-                    formula.set(i + 1, String.valueOf(a - b));
-                } else if (formula.get(i).equals("MUL")) {
-                    formula.set(i + 1, String.valueOf(a * b));
-                } else if (formula.get(i).equals("DIV")) {
-                    formula.set(i + 1, String.valueOf(a / b));
+            if (formula.size() >= 3) {
+                for (int i = 1; i <= formula.size() - 1; i += 2) {
+                    float a = 0;
+                    float b = 0;
+                    try {
+                        a = Float.parseFloat(formula.get(i - 1));
+                        b = Float.parseFloat(formula.get(i + 1));
+                    } catch (NumberFormatException e) {
+                        numbersBox.setText("error");
+                    }
+                    Log.i(LOG_TAG, formula.toString());
+                    Log.i(LOG_TAG, a + " " + formula.get(i) + " " + b);
+                    if (formula.get(i).equals("ADD")) {
+                        formula.set(i + 1, String.valueOf(Float.sum(a, b)));
+                    } else if (formula.get(i).equals("SUB")) {
+                        formula.set(i + 1, String.valueOf(a - b));
+                    } else if (formula.get(i).equals("MUL")) {
+                        formula.set(i + 1, String.valueOf(a * b));
+                    } else if (formula.get(i).equals("DIV")) {
+                        formula.set(i + 1, String.valueOf(a / b));
+                    }
+                    Log.i(LOG_TAG, formula.toString());
                 }
-                Log.i(LOG_TAG, formula.toString());
+                numbersBox.setText(formula.getLast());
             }
-            numbersBox.setText(formula.getLast());
+
             formula.clear();
         });
-
     }
 
     @Override
